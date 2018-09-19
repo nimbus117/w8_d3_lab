5b03f9e28938ac16371bcd09ee832204b63d6566
nimbus117 <mail@nimbus117.co.uk>
2018-09-19 21:38:37 +0100

    general refactoring, chained promises

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,36 +1,21 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
-const createRouter = function(collection){
+const createRouter = function (collection) {
 
   const router = express.Router();
 
-  const addTimestamp = function(doc){
-    doc['created_date'] = doc._id.getTimestamp().toGMTString().substring(0,16);
+  const addTimestamp = function (doc) {
+    doc['created_date'] = doc._id
+      .getTimestamp().toGMTString().substring(0,16);
     return doc;
   };
 
   //Show All
   router.get('/', (req, res) => {
-    collection
-      .find()
-      .toArray()
-      .then((docs) => res.json(docs.map(addTimestamp)))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-    });
-  });
-
-  //Show by ID
-  router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    collection
-      .find({ _id: ObjectID(id) })
-      .toArray()
-      .then((docs) => res.json(docs))
-      .catch((err) => {
+    collection.find().toArray()
+      .then(docs => res.json(docs.map(addTimestamp)))
+      .catch(err => {
         console.error(err);
         res.status(500);
         res.json({ status: 500, error: err });
@@ -38,32 +23,23 @@ const createRouter = function(collection){
   });
 
   //Create
-  router.post('/',(req, res) => {
-    const newData = req.body;
-    collection
-      .insertOne(newData)
-      .then(() => {
-        collection
-        .find()
-        .toArray()
-      .then((docs) => {
-        res.json(docs.map(addTimestamp));
-      });
+  router.post('/', (req, res) => {
+    collection.insertOne(req.body)
+      .then(() => collection.find().toArray())
+      .then(docs => res.json(docs.map(addTimestamp)))
+      .catch(err => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
       });
   });
 
   //Delete
   router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    collection
-      .deleteOne({ _id: ObjectID(id) })
-      .then(() => {
-        collection
-          .find()
-          .toArray()
-          .then((docs) => res.json(docs.map(addTimestamp)));
-      })
-      .catch((err) => {
+    collection.deleteOne({_id: ObjectID(req.params.id)})
+      .then(() => collection.find().toArray())
+      .then(docs => res.json(docs.map(addTimestamp)))
+      .catch(err => {
         console.error(err);
         res.status(500);
         res.json({ status: 500, error: err });
@@ -72,15 +48,14 @@ const createRouter = function(collection){
 
   //Update
   router.put('/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedData = req.body;
-
-    collection.updateOne(
-      { _id: ObjectID(id) },
-      { $set: updatedData}
-    )
-    .then(() => collection.find().toArray())
-    .then((docs) => res.json(docs.map(addTimestamp)));
+    collection.updateOne({_id: ObjectID(req.params.id)}, {$set: req.body})
+      .then(() => collection.find().toArray())
+      .then(docs => res.json(docs.map(addTimestamp)))
+      .catch(err => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
   });
 
   return router;
