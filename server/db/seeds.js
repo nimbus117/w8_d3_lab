f0b84ad5a0d4a9966dd8e375a52c8285d3c48f97
kaluzsikc <kaluzsikc@gmail.com>
2018-09-19 10:17:37 +0100

    Initial commit

---
+++ server/db/seeds.js
@@ -0,0 +1,21 @@
+use bucket;
+db.dropDatabase();
+
+db.list.insertMany([
+  {
+    goal: "Swim with dolphins",
+    completed_date:""
+  },
+  {
+    goal: "Visit China",
+    completed_date:""
+  },
+  {
+    goal: "Learn Dutch",
+    completed_date:""
+  },
+  {
+    goal: "Climb a rock wall",
+    completed_date:""
+  }
+])
